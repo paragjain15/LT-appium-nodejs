ce5d611921272e3ea2f5e9e21ea2fb786b1b609f
PARAG JAIN <87814011+paragjain15@users.noreply.github.com>
2022-08-23 13:02:28 +0530

    Update Android.js

--- Android.js
+++ Android.js
@@ -18,7 +18,7 @@ const deviceVersion = process.env.LT_DEVICE_VERSION;
  */
 const desiredCapabilities = {
   app: appId, // Enter the 'app_url' here.
-  build: buildName,
+//   build: buildName,
   name: "Sample Test NodeJS",
   deviceName: deviceName,
   isRealMobile: true,
